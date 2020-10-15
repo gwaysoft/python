2a9667a07bf090a0db0ec023092dfba1622a5365
gwaysoft <gwaysoft@outlook.com>
2020-10-15 11:52:34 +0800

    modify

---
+++ standard/logging/gspackage/utils/config.py
@@ -0,0 +1,19 @@
+def getAbsFile(filename="config.ini"):
+    import os
+    currentDir = os.path.abspath(os.path.dirname(__file__))
+    fatherDir = os.path.dirname(currentDir)
+    print(fatherDir + "/" + filename)
+    return fatherDir + "/" + filename
+
+
+def getConfig(fileName="config.ini"):
+    import configparser
+    config = configparser.ConfigParser(allow_no_value=True)
+    config.read(getAbsFile(fileName))
+    return config
+
+
+def getValue(fileName="../config.ini", section="DEFAULT", key="top"):
+    config = getConfig(fileName)
+    return config[section][key]
+
