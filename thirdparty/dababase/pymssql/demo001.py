6cef5d5ca0213a858cbb92517c97a5b405f58d24
gwaysoft <gwaysoft@outlook.com>
2020-10-16 09:59:49 +0800

    modify

--- thirdparty/dababase/pymssql/demo001.py
+++ thirdparty/dababase/pymssql/demo001.py
@@ -4,12 +4,13 @@ conn = pymssql.connect(host='172.16.31.201',
                        user='sa',
                        password='eBao2014',
                        database='spiratest',
-                       charset='utf8')
+                       charset='utf8', port='1433', as_dict=False)
 
 
 # 查看连接是否成功
 cursor = conn.cursor()
-sql = 'select * from user'
+sql = 'select * from TST_USER'
+# sql = 'select * from TST_PROJECT'
 cursor.execute(sql)
 # 用一个rs变量获取数据
 rs = cursor.fetchall()
---
+++ thirdparty/dababase/pymssql/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# c
+yum -y install gcc
+# c++
+yum -y install gcc-c++
+
